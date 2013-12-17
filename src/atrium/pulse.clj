9e3784ccadf5591eaeba20bfbe2ac74d7257d733
Michael Drogalis <mjd3089@rit.edu>
2013-12-16 20:04:10 -0500

    Gather into launcher.

--- src/atrium/pulse.clj
+++ src/atrium/pulse.clj
@@ -5,50 +5,47 @@
   (:import [org.apache.zookeeper KeeperException$BadVersionException]
            [org.apache.zookeeper KeeperException$NoNodeException]))
 
-(def polling-frequency 1000)
-
-(def pulse-frequency 800)
-
 (defn serialize-edn [x]
   (.getBytes (pr-str x)))
 
 (defn deserialize-edn [x]
   (read-string (String. x "UTF-8")))
 
-(def client (zk/connect "127.0.0.1:2181"))
-
-(def master-node "/master")
+(def pulse-path "/atrium")
 
 (defn pulse-node [id]
-  (str "/" id))
+  (str pulse-path "/" id))
 
-(defn create-master-node []
-  (zk/create client master-node :persistent? true))
+(defn create-pulse-path [client]
+  (zk/create client pulse-path :persistent? true))
 
-(defn create-pulse-node [id]
+(defn create-pulse-node [client id]
   (zk/create client (pulse-node id))
   (let [version (:version (zk/exists client (pulse-node id)))]
     (zk/set-data client (pulse-node id) (serialize-edn {:pulse 0}) version)))
 
+(defn create-master-node [client node]
+  (zk/create client node :persistent? true))
+
 (defn monitor-for-failure
-  ([master-id] (monitor-for-failure master-id nil 0))
-  ([master-id previous-version missed-beats]
+  ([master-id config client] (monitor-for-failure master-id config client nil 0))
+  ([master-id config client prev-ver missed-beats]
      (if (>= missed-beats 2)
        (throw (ex-info "Master pulse stopped" {:master master-id}))
-       (do (<!! (timeout polling-frequency))
+       (do (<!! (timeout (:polling-frequency config)))
            (let [payload (zk/data client (pulse-node master-id))
                  version (:version (:stat payload))]
-             (if (not= version previous-version)
+             (if (not= version prev-ver)
                (do (println "Saw the heart beat at version" version)
-                   (recur master-id version 0))
+                   (recur master-id config client version 0))
                (do (println "Saw a missed heart beat at version" version)
-                   (recur master-id version (inc missed-beats)))))))))
+                   (recur master-id config client version (inc missed-beats)))))))))
 
-(defn stand-by [payload ch]
+(defn stand-by [config client payload ch]
   (let [master-id (:id (deserialize-edn (:data payload)))]
-    (monitor-for-failure master-id)))
+    (monitor-for-failure master-id config client)))
 
-(defn trigger-rendezvous [e payload ch]
+(defn trigger-rendezvous [e config client payload ch]
   (>!! ch (:version (:stat payload))))
 
 (d/with-handler! #'stand-by
@@ -63,59 +60,67 @@
   KeeperException$NoNodeException
   trigger-rendezvous)
 
-(defn elect-self [id version]
-  (let [node (zk/data client master-node)]
-    (zk/set-data client master-node (serialize-edn {:id id}) version)
+(defn elect-self [client {:keys [master-path id]} version]
+  (let [node (zk/data client master-path)]
+    (zk/set-data client master-path (serialize-edn {:id id}) version)
     true))
 
 (d/with-handler! #'elect-self
   KeeperException$BadVersionException
-  (fn [e id version]
-    (println id "was beat by another node in becoming the master")
+  (fn [e client config version]
+    (println (:id config) "was beat by another node in becoming the master")
     false))
 
-(d/with-post-hook! #'elect-self
-  (fn [_] (println "Mastery acquired.")))
-
-(defn beat [id master {:keys [data stat] :as pulse-data}]
+(defn beat [client id master {:keys [data stat] :as pulse-data}]
   (zk/set-data client (pulse-node id) data (:version stat)))
 
-(defn heart-beat-process [id]
-  (future
-    (while true
-      (let [master (zk/data client master-node)
-            {:keys [data stat]} (zk/data client (pulse-node id))]
-        (beat id master (zk/data client (pulse-node id))))
-      (<!! (timeout pulse-frequency)))))
+(defn heart-beat-process [client {:keys [id master-path pulse-frequency]}]
+  (thread
+   (while true
+     (let [master (zk/data client master-path)
+           {:keys [data stat]} (zk/data client (pulse-node id))]
+       (beat client id master (zk/data client (pulse-node id))))
+     (<!! (timeout pulse-frequency)))))
+
+(defn launch! [{:keys [host port id master-path] :as config}]
+  (let [user-ch (chan)
+        client (zk/connect (str host ":" port))]
+    (create-pulse-path client)
+    (create-master-node client master-path)
+    (create-pulse-node client id)
+    (thread
+     (loop [rendezvous (chan)]
+       (let [payload (zk/data client master-path)]
+         (thread (stand-by config client payload rendezvous))
+         (let [version (<!! rendezvous)]
+           (if (elect-self client config version)
+             (do (heart-beat-process client config)
+                 (>!! user-ch true))
+             (recur (chan)))))))
+    user-ch))
 
 (d/with-precondition! #'beat
   :still-master
-  (fn [id master pulse-data]
+  (fn [client id master {:keys [data stat] :as pulse-data}]
     (= (:id (deserialize-edn (:data master))) id)))
 
 (d/with-handler! #'beat
   {:precondition :still-master}
-  (fn [e id _ _] (println id "is not the master anymore. Dying")))
-
-(d/with-pre-hook! #'stand-by
-  (fn [payload ch] (println "On stand-by")))
-
-(d/with-pre-hook! #'elect-self
-  (fn [id version] (println id "is attempting to become the master")))
+  (fn [e client id master {:keys [data stat] :as pulse-data}]
+    (println id "is not the master anymore. Dying")))
 
 (d/with-pre-hook! #'beat
-  (fn [id _ _] (println id "heart beats")))
+  (fn [client id master {:keys [data stat] :as pulse-data}]
+    (println id "heart beats")))
 
-(defn boot! [id]
-  (create-master-node)
-  (create-pulse-node id)
-  (loop [rendezvous (chan)]
-    (let [payload (zk/data client master-node)]
-      (future (stand-by payload rendezvous))
-      (let [version (<!! rendezvous)]
-        (if (elect-self id version)
-          (heart-beat-process id)
-          (recur (chan)))))))
+(d/with-pre-hook! #'stand-by
+  (fn [config client payload ch]
+    (println "On stand-by")))
 
+(d/with-pre-hook! #'elect-self
+  (fn [client {:keys [id master-path]} version]
+    (println id "is attempting to become the master")))
 
+(d/with-post-hook! #'elect-self
+  (fn [_] (println "Taking over as the master")))
 
